481494f39ff0e1c993ea3de444705bb1c42743e1
nurulsucisawaliah <71654996+nurulsucisawaliah@users.noreply.github.com>
2020-11-29 18:46:20 +0800

    Create latihan2.py

---
+++ latihan2.py
@@ -0,0 +1,24 @@
+input('Masukan nama file : ')
+file = open("dataMhs.txt", 'a')
+file.write("xxxx")
+file.close()
+
+file = open("dataMhs.txt", 'a')
+file.write("\ny")
+file.close()
+
+file = open("dataMhs.txt", 'a')
+file.write("\nxxxx")
+file.close()
+
+file = open("dataMhs.txt", 'a')
+file.write("\ny")
+file.close()
+
+file = open("dataMhs.txt", 'a')
+file.write("\nxxxx")
+file.close()
+
+file = open("dataMhs.txt", 'a')
+file.write("\nn")
+file.close()
