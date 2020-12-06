5a736040532c69505c8a6c3eac9af9b8ebf039d3
nurulsucisawaliah <71654996+nurulsucisawaliah@users.noreply.github.com>
2020-12-06 14:10:24 +0800

    Create Data.py

---
+++ Data.py
@@ -0,0 +1,17 @@
+# membuka dan mau membaca file d:/Dataa.txt
+try:
+    file = open("c:/data.txt", "r")
+except FileNotFoundError :
+    print('file tidak ditemukan')
+
+# baca baris pertama dari file
+# simpan kedalam variabel bil1 sbg integer
+try:
+    bil1 = int(file.readline())
+    bil2 = int(file.readline())
+
+#hitung dan tampilkan hasil bagi
+    hasil = bil1/ bil2
+    print(bil1, 'dibagi ',bil2, ' sama dengan ',hasil)
+except :
+    print('Tidak boleh pembagian dengan nol')
